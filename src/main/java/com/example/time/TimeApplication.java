57129030a34c322c22e370de4757bda56bd843e5
patriciakasapa <patriciakasapa07@gmail.com>
2020-01-08 15:11:45 +0000

    Init

---
+++ src/main/java/com/example/time/TimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/time/TimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
