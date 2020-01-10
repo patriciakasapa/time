decf803e34ded0fa6d1de0f43d5e6ec98802a82c
pkerbynn <john.erbynn@gmail.com>
2020-01-10 18:51:30 +0000

    empty response

--- src/main/java/com/example/time/controllers/EmployeeController.java
+++ src/main/java/com/example/time/controllers/EmployeeController.java
@@ -11,45 +11,35 @@ import org.springframework.web.bind.annotation.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 @Api
 @RestController
+@CrossOrigin(origins = "*")
 public class EmployeeController {
-    public static final DateFormat DATE_FORMAT = new SimpleDateFormat("MM d, yyyy", Locale.ENGLISH);
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     @Autowired
     private EmployeeServiceImpl employeeService;
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @ApiOperation("get all availableEmployees")
-    @ResponseStatus(value = HttpStatus.OK)
-    @GetMapping({"/availableEmployees", "/"})
+    @GetMapping(value = "/availableEmployees/{projectStartDate}/{projectEndDate}", produces = "application/json")
     public List<Employee> getAllAvailableEmployees(
-            @PathVariable String projectStartDate,
-            @PathVariable String projectEndDate
+            @PathVariable("projectStartDate") String projectStartDate,
+            @PathVariable("projectEndDate") String projectEndDate
     ){
-        Date leaveStartDate = null;
-        Date leaveEndDate = null;
-        String sDate2 = "31-Dec-2019";
-        try {
-            leaveEndDate = DATE_FORMAT.parse(projectEndDate);
-            leaveStartDate = DATE_FORMAT.parse(projectStartDate);
+         try {
+            Date start = DATE_FORMAT.parse(projectStartDate);
+            Date end = DATE_FORMAT.parse(projectEndDate);
+             System.out.println(start);
+             System.out.println(end);
+            return employeeService.getAllAvailableEmployees(start, end);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
-        if (leaveEndDate == null && leaveStartDate == null) {
-            try {
-                leaveEndDate = DATE_FORMAT.parse(sDate2);
-                leaveStartDate = DATE_FORMAT.parse(sDate2);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return employeeService.getAllAvailableEmployees(leaveStartDate, leaveEndDate);
+        return new ArrayList<>();
     }
 }
